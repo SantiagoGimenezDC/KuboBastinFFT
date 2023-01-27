9fdc358c91842b0803cb3cd7204ea2f7477609e9
SantiagoGimenezDC <santiago.gdc@gmail.com>
2023-01-27 23:44:59 +0000

    27/01/23

--- build/src/main.d
+++
@@ -1,4 +0,0 @@
-build/src/main.o: src/main.cpp src/static_vars.hpp \
- src/KuboBastin_solver.hpp
-src/static_vars.hpp:
-src/KuboBastin_solver.hpp:
--- build/src/update_cheb.d
+++
@@ -1,4 +0,0 @@
-build/src/update_cheb.o: src/update_cheb.cpp src/static_vars.hpp \
- src/update_cheb.hpp
-src/static_vars.hpp:
-src/update_cheb.hpp:
