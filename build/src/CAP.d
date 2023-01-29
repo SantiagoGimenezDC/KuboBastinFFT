11875fbcec75fc6262380fb77ab6d1d37c430111
SantiagoGimenezDC <santiago.gdc@gmail.com>
2023-01-29 22:49:21 +0000

    29/01/2023

--- build/src/CAP.d
+++
@@ -1,3 +0,0 @@
-build/src/CAP.o: src/CAP.cpp src/static_vars.hpp src/CAP.hpp
-src/static_vars.hpp:
-src/CAP.hpp:
--- build/src/vec_base.d
+++
@@ -1,4 +0,0 @@
-build/src/vec_base.o: src/vec_base.cpp src/static_vars.hpp \
- src/vec_base.hpp
-src/static_vars.hpp:
-src/vec_base.hpp:
