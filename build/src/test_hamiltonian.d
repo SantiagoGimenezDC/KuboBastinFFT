29a07e64290aeb9eba062541ed5349ff62ba350d
SantiagoGimenezDC <santiago.gdc@gmail.com>
2023-02-14 00:04:41 +0000

    14/02/23

--- build/src/test_hamiltonian.d
+++
@@ -1,5 +0,0 @@
-build/src/test_hamiltonian.o: src/test_hamiltonian.cpp \
- src/static_vars.hpp src/update_cheb.hpp src/test_hamiltonian.hpp
-src/static_vars.hpp:
-src/update_cheb.hpp:
-src/test_hamiltonian.hpp:
